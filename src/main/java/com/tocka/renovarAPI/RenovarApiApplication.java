832e0e7f6d9ba68a79b294008906c753894f3426
Vitor Andrade <150646534+Tockinha123@users.noreply.github.com>
2026-01-05 03:20:21 -0300

    chore: init project

---
+++ src/main/java/com/tocka/renovarAPI/RenovarApiApplication.java
@@ -0,0 +1,13 @@
+package com.tocka.renovarAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovarApiApplication.class, args);
+	}
+
+}
